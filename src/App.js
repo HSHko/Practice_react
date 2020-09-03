a228b49b71931a3082abc1c2c363d5bd27b7bff3
pleasmin@gmail.com <pleasmin@gmail.com>
2020-09-04 00:17:56 +0900

    practice_new_file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
+class App extends Component {
+  render() {
+    
+    return (
+      <React.Fragment>
+        <div className="App">
+          Hello, World!
+          <Person name="Max" age="28"/>
+          <Person name="Babi" age="16"/>
+          <Person name="Cold" age="32"/>
+        </div>
+        
+      </React.Fragment>
+      
+    )
+    
+  }
+}
+
+export default App;
+
+/*
 function App() {
   return (
     <div className="App">
@@ -24,3 +47,4 @@ function App() {
 }
 
 export default App;
+*/
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const person = () => {
+  return (
+    <p>
+      I'm a Person X Years Old!
+    </p>
+  )
+}
+
+
+export default person;
